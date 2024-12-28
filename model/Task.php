3ae1a95880072dee6c310b1404ad0be04096f2f9
achraf <achrafsikal@gmail.com>
2024-12-28 23:20:27 +0100

    include model\task.php in the add

--- model/Task.php
+++ model/Task.php
@@ -1,11 +1,33 @@
-<?
-session_start();
+<?php
 
-include("../classes/Database.php");
+include __DIR__ ."/../classes/Database.php";
 include_once __DIR__ . "/../config/config.php";
 
-class listTask extends connect{
+
+class ModelTask extends connect {
+    public function insertTask($title, $description) {
+        $query = "INSERT INTO Tasks (title, description) VALUES (?, ?)";
+        return $this->execute($query, [$title, $description]);
+    }
+    // public function assignTask($email) {
+    //     $query = "INSERT INTO Tasks (title, description) VALUES (?, ?)";
+    //     return $this->execute($query, [$title, $description]);
+    // }
     public function getTasks($email) {
         return $this->fetch(SQL_TASKS, [$email], false);
     }
 }
+// $model = new ModelTask();
+// $result = $model->insertTask("Title of Task", "Description of Task", "To Do", "Simple");
+// $tasks = $model->getTasks();
+
+// if ($result) {
+//     echo "Task inserted successfully!";
+//     var_dump($result);
+// } else {
+//     echo "Error inserting task.";
+// }
+
+// echo "<pre>";
+//     print_r($tasks);
+// echo "</pre>";
