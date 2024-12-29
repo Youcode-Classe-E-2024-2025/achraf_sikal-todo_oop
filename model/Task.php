7bbe22c873cc4c4031b8b36a25ed8593b7fc5866
achraf <achrafsikal@gmail.com>
2024-12-29 14:19:14 +0100

    staging task.php

--- model/Task.php
+++ model/Task.php
@@ -5,14 +5,17 @@ include_once __DIR__ . "/../config/config.php";
 
 
 class ModelTask extends connect {
-    public function insertTask($title, $description) {
+    public function insertTask($title, $description, $email) {
+        $this->db->beginTransaction();
         $query = "INSERT INTO Tasks (title, description) VALUES (?, ?)";
-        return $this->execute($query, [$title, $description]);
+        $this->execute($query, [$title, $description]);
+        $taskId = $this->db->lastInsertId();
+        $user = $this->fetch("SELECT user_id FROM users WHERE email = ?;", [$email]);
+        $userId = $user[0]["user_id"];
+        $this->execute("INSERT INTO usertasks (user_id, task_id) VALUES (?, ?)", [$userId, $taskId]);
+        $this->db->commit();
+        return true;
     }
-    // public function assignTask($email) {
-    //     $query = "INSERT INTO Tasks (title, description) VALUES (?, ?)";
-    //     return $this->execute($query, [$title, $description]);
-    // }
     public function getTasks($email) {
         $result = $this->fetch(SQL_TASKS, [$email], false);
         if ($result) {
@@ -22,17 +25,3 @@ class ModelTask extends connect {
         }
     }
 }
-// $model = new ModelTask();
-// $result = $model->insertTask("Title of Task", "Description of Task", "To Do", "Simple");
-// $tasks = $model->getTasks();
-
-// if ($result) {
-//     echo "Task inserted successfully!";
-//     var_dump($result);
-// } else {
-//     echo "Error inserting task.";
-// }
-
-// echo "<pre>";
-//     print_r($tasks);
-// echo "</pre>";
